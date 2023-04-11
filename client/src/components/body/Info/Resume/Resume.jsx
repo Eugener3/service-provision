437c1d00ac7130d464a6eb5802288a9a78ae6f78
disa4148 <denis.nikolaenko.2004@mail.ru>
2023-04-12 05:44:53 +0700

    add a avatar upload

--- client/src/components/body/Info/Resume/Resume.jsx
+++ client/src/components/body/Info/Resume/Resume.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 
 import styles from "./Resume.module.scss";
 
@@ -8,70 +8,83 @@ import profileDefault from "../../../../img/photos/profile-default.jpg";
 
 export const Resume = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
+  const [image, setImage] = useState(localStorage.getItem("image"));
 
-  
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
-    setSelectedFile(selectedFile);
-    console.log(selectedFile);
-  
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      localStorage.setItem("image", reader.result);
+      setImage(reader.result);
+    };
+    reader.readAsDataURL(selectedFile);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    setSelectedFile(null)
-  }
+    setSelectedFile(null);
+    setImageUrl("");
+  };
 
-  // onSubmit={handleSubmit}
   return (
     <>
       <p className={styles.addMore}>Заполните свои данные</p>
       <form action="" onSubmit={handleSubmit}>
-      <div className={styles.wrapper}>
-        <div className={styles.inputsWrapper}>
-          
+        <div className={styles.wrapper}>
+          <div className={styles.inputsWrapper}>
             <div className={styles.form}>
-            <input type="text" placeholder="ФИО" />
-            <input type="text" placeholder="Телефон" />
-            <input type="text" placeholder="E-mail" />
-            <textarea
-              name="description"
-              id=""
-              cols="30"
-              rows="10"
-              placeholder="О себе"
-            ></textarea>
+              <input type="text" placeholder="ФИО" />
+              <input type="text" placeholder="Телефон" />
+              <input type="text" placeholder="E-mail" />
+              <textarea
+                name="description"
+                id=""
+                cols="30"
+                rows="10"
+                placeholder="О себе"
+              ></textarea>
             </div>
-          
-        </div>
-        <div>
-          <img
-            className={styles.Avatar}
-            src={profileDefault}
-            alt="profileDefault"
-          />
-          <div class="input__wrapper">
-            
-              <input type="file" onChange={handleFileChange} name="file" id="file" class="input__file" />
-            
-            {selectedFile &&  <img src={selectedFile} alt="" style={{ width: "300px" }} /> }
-            <label for="file" class="input__file-button">
-              <span class="input__file-icon-wrapper">
-                <img
-                  class="input__file-icon"
-                  src={require("../../../../img/photos/icons-download.png")}
-                  alt="Выбрать файл"
-                  width="25"
-                  height="25"
-                />
-              </span>
-              <span class="input__file-button-text">Выберите файл</span>
-            </label>
           </div>
-          <button>Сохранить</button>
+          <div>
+            {image ? (
+              <img
+                className={styles.Avatar}
+                src={image}
+                alt="selectedFile"
+              />
+            ) : (
+              <img
+                className={styles.Avatar}
+                src={profileDefault}
+                alt="profileDefault"
+              />
+            )}
+            <div class="input__wrapper">
+              <input
+                type="file"
+                onChange={handleFileChange}
+                name="file"
+                id="file"
+                class="input__file"
+              />
+              <label for="file" class="input__file-button">
+                <span class="input__file-icon-wrapper">
+                  <img
+                    class="input__file-icon"
+                    src={require("../../../../img/photos/icons-download.png")}
+                    alt="Выбрать файл"
+                    width="25"
+                    height="25"
+                  />
+                </span>
+                <span class="input__file-button-text">Выберите файл</span>
+              </label>
+            </div>
+            <button>Сохранить</button>
+          </div>
         </div>
-      </div>
       </form>
     </>
   );
